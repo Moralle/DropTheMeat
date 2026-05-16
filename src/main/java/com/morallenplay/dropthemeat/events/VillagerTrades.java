df4b35330d17e65a099aaba4981bcac1e378af29
Moralle <29687693+Moralle@users.noreply.github.com>
2026-05-16 16:00:39 +0200

    final 1.20.1 bugfix

--- src/main/java/com/morallenplay/dropthemeat/events/VillagerTrades.java
+++ src/main/java/com/morallenplay/dropthemeat/events/VillagerTrades.java
@@ -86,7 +86,7 @@ public class VillagerTrades {
 				event.getTrades().get(5).add(new BasicItemListing(new ItemStack(ItemInit.RAW_SNIFFER.get(), 4), new ItemStack(Items.EMERALD, 1), 16, 30, 0.05f));
 			}
 			if (Config.SNOW_GOLEM_TRADE.get()) {
-				event.getTrades().get(5).add(new BasicItemListing(new ItemStack(ItemInit.RAW_SNIFFER.get(), 4), new ItemStack(Items.EMERALD, 1), 16, 30, 0.05f));
+				event.getTrades().get(5).add(new BasicItemListing(new ItemStack(ItemInit.FROZEN_GOLEM_NUGGET.get(), 4), new ItemStack(Items.EMERALD, 1), 16, 4, 0.05f));
 			}
 		}
 	}
